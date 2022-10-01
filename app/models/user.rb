2ce772df7e944d8f9688e65423fb54c5c48ff707
felipe <felipe.pinto.2912@gmail.com>
2022-10-01 15:10:53 -0300

    Added CSS to user sign in and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :event_attendances, foreign_key: :attendee_id # join table
   has_many :attended_events, through: :event_attendances, source: :attended_event
 
-  validates :username, :password, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :username, :password, :password_confirmation, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }
 
   scope :accepted, -> { where('status = 1') }
   scope :invited, -> { where('status = 0') }
