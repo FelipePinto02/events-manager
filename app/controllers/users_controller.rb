26818ecbfe42074d9f3a4516bc037d6fe035714e
felipe <felipe.pinto.2912@gmail.com>
2022-10-05 19:21:47 -0300

    Improved Users #index and #show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @event_id = params[:event_id]
 
     redirect_to root_path if params[:event_id].nil?
   end
 
   def show
     @user = User.find(params[:id])
+    @future_events = @user.attended_events.future
+    @past_events = @user.attended_events.past
   end
 end
